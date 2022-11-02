fa05341b1147f46a3d284294dc23eec3202f78e3
Lucas Rosén <lucasrosen.dev@gmail.com>
2022-11-02 18:21:24 +0100

    Move grid generation to function generateGrid()

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const gridSize = 16;
-
 const gridContainer = document.querySelector("#grid-container");
 
-/* Columns */
-for (i = 0; i < gridSize; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    gridContainer.appendChild(column);
+function generateGrid(gridSize) {
+    /* Columns */
+    for (i = 0; i < gridSize; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        gridContainer.appendChild(column);
 
-    /* Rows */
-    for (j = 0; j < gridSize; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        column.appendChild(square);
+        /* Rows */
+        for (j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            column.appendChild(square);
+        }
     }
 }
+
+generateGrid(16);
